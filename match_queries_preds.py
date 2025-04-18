a733ffb3cbd92f43f113915bd8048f8476e84a9b
FarInHeight <105938333+FarInHeight@users.noreply.github.com>
2025-04-18 09:33:27 +0200

    added uncertainty evaluation and refactored some code portions

--- match_queries_preds.py
+++ match_queries_preds.py
@@ -8,7 +8,9 @@ from tqdm import tqdm
 from pathlib import Path
 from copy import deepcopy
 
-sys.path.append(str(Path("image-matching-models")))
+from util import read_file_preds
+
+sys.path.append(str(Path(__file__).parent.joinpath("image-matching-models")))
 
 from matching import get_matcher, available_models
 from matching.utils import get_default_device
@@ -17,7 +19,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     
     parser.add_argument("--preds-dir", type=str, help="directory with predictions of a VPR model")
-    parser.add_argument("--out-dir", type=str, default=None, help="output directory")
+    parser.add_argument("--out-dir", type=str, default=None, help="output directory of image matching results")
     # Choose matcher
     parser.add_argument(
         "--matcher",
@@ -28,7 +30,7 @@ def parse_arguments():
     )
     parser.add_argument("--device", type=str, default=get_default_device(), choices=["cpu", "cuda"])
     parser.add_argument("--im-size", type=int, default=512, help="resize img to im_size x im_size")
-    parser.add_argument("--num-preds", type=int, default=100, help="number of predictions")
+    parser.add_argument("--num-preds", type=int, default=100, help="number of predictions to match")
     parser.add_argument("--start-query", type=int, default=-1, help="query to start from")
     parser.add_argument("--num-queries", type=int, default=-1, help="number of queries")
 
@@ -43,10 +45,11 @@ def main(args):
     preds_folder = args.preds_dir
     start_query = args.start_query
     num_queries = args.num_queries
-    output_folder = preds_folder + f"_{matcher_name}" if args.out_dir is None else args.out_dir
 
-    os.makedirs(output_folder, exist_ok=True)
-    txt_files = glob(f"{preds_folder}/*.txt")
+    output_folder = Path(preds_folder + f"_{matcher_name}") if args.out_dir is None else Path(args.out_dir)
+    output_folder.mkdir(exist_ok=True)
+    
+    txt_files = glob(os.path.join(preds_folder, "*.txt"))
     txt_files.sort(key=lambda x: int(Path(x).stem))
 
     start_query = start_query if start_query >= 0 else 0
@@ -54,21 +57,18 @@ def main(args):
 
     for txt_file in tqdm(txt_files[start_query : start_query + num_queries]):
         q_num = Path(txt_file).stem
-        if os.path.exists(f"{output_folder}/{q_num}.torch"):
+        out_file = output_folder.joinpath(f"{q_num}.torch")
+        if out_file.exists():
             continue
         results = []
-        with open(txt_file) as file:
-            lines = file.read().splitlines()
-        q_path = lines[1]
-        pred_paths = lines[4:104]
-        assert pred_paths[0][:5] == pred_paths[-1][:5]
+        q_path, pred_paths = read_file_preds(txt_file)
         img0 = matcher.load_image(q_path, resize=img_size)
         for pred_path in pred_paths[:num_preds]:
             img1 = matcher.load_image(pred_path, resize=img_size)
             result = matcher(deepcopy(img0), img1)
             result["all_desc0"] = result["all_desc1"] = None
             results.append(result)
-        torch.save(results, f"{output_folder}/{q_num}.torch")
+        torch.save(results, out_file)
 
 if __name__ == "__main__":
     args = parse_arguments()
